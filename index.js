489eb9acbd51b720a9a38e1740b3ee32d082f98f
fxhxyz4 <fxhxyz@proton.me>
2023-06-06 23:03:15 +0300

    edit index.js

--- index.js
+++ index.js
@@ -22,11 +22,13 @@ import WebSocket from 'ws';
 
 const ws = new WebSocket('wss://irc-ws.chat.twitch.tv:443');
 
-const user = 'fxhxyz'; // your twitch username
-const oauth = ''; // your twitch OAuth token https://twitchapps.com/tmi/
+const user = 'bxffz_'; // your twitch username
+const oauth = 'oauth:asuw2z5qiwz5ke72tamg1objh5lh98'; // your twitch OAuth token https://twitchapps.com/tmi/
 
-const delay = 3000; // Delay for changing color in chat (recommend min min: 3s/3000ms)
-const colors = [
+const delay = 8000; // Delay for changing color in chat (recommend min delay: 5s/5000ms)
+const customColors = []; // your custom colors
+
+const defaultColors = [
   '#FF0000',
   '#008000',
   '#0000FF',
@@ -48,21 +50,40 @@ const prime = false; // if you have Twitch Prime or Turbo, set to true
 const getColor = () => {
   if (prime) {
     return '#' + ((Math.random() * 0xffffff) << 0).toString(16);
+  } else if (customColors && customColors.length > 0) {
+    return customColors[Math.floor(Math.random() * customColors.length)];
   } else {
-    return colors[Math.floor(Math.random() * colors.length)];
+    return defaultColors[Math.floor(Math.random() * defaultColors.length)];
   }
 };
 
 const TwitchColorChanger = () => {
   const color = getColor();
-  ws.send(`PRIVMSG #${user} :/color ${color}`);
+
+  if (ws.readyState === WebSocket.OPEN) {
+    ws.send(`PRIVMSG #${user} :/color ${color}`);
+    console.info(`new color: ${color}`);
+  }
 };
 
 ws.on('open', () => {
+  console.info(`ws connected`.green);
+
   ws.send(`PASS ${oauth}`);
   ws.send(`NICK ${user}`);
   ws.send(`JOIN #${user}`);
 });
+
+const showSettings = () => {
+  console.log(`==================Settings==================`.red);
+  console.log('--user: '.red + `${user}`.magenta);
+  console.log('-token: '.red + `${oauth}`.magenta);
+  console.log('-delay: '.red + `${delay}`.magenta);
+  console.log('-prime: '.red + `${prime}`.magenta);
+  console.log(`==================Settings==================`.red);
+};
+
+showSettings();
 
 setInterval(() => {
   TwitchColorChanger();
